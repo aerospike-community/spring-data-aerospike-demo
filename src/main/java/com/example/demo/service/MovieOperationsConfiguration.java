ec59440bd85e7e6c751836d706e9bb82c199f48a
asmirnova <asmirnova@playtika.com>
2021-07-30 19:45:51 +0300

    CAS documentation

--- src/main/java/com/example/demo/service/MovieOperationsConfiguration.java
+++ src/main/java/com/example/demo/service/MovieOperationsConfiguration.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.aerospike.core.AerospikeTemplate;
 
 @Configuration
-public class OperationsConfiguration {
+public class MovieOperationsConfiguration {
 
     @Bean
     public MovieOperations movieOperations(MovieRepository repository,
