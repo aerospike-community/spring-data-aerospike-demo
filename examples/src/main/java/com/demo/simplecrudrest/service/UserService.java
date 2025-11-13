255f9af95df77bc9f303caf1ebeca32da00104d4
agrgr <agrgr@users.noreply.github.com>
2025-11-13 14:11:19 +0100

    update dependencies, add log messages

--- examples/src/main/java/com/demo/simplecrudrest/service/UserService.java
+++ examples/src/main/java/com/demo/simplecrudrest/service/UserService.java
@@ -3,6 +3,7 @@ package com.demo.simplecrudrest.service;
 import com.demo.simplecrudrest.entity.User;
 import com.demo.simplecrudrest.repository.UserRepository;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,19 +13,23 @@ import java.util.stream.StreamSupport;
 
 @Service
 @AllArgsConstructor
+@Slf4j
 public class UserService {
 
     UserRepository aerospikeUserRepository;
 
     public Optional<User> findUserById(int id) {
+        log.info("Looking for a user with id {}", id);
         return aerospikeUserRepository.findById(id);
     }
 
     public void addUser(User user) {
+        log.info("Saving user with id {}", user.getId());
         aerospikeUserRepository.save(user);
     }
 
     public void removeUserById(int id) {
+        log.info("Deleting user with id {}", id);
         aerospikeUserRepository.deleteById(id);
     }
 
